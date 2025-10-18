4483e92a4db05c6b37a63ce87e095686f93e4f16
Yunus Ahmed <YunusAhmed1266@gmail.com>
2025-10-18 09:00:40 +0100

    add functionality to sidebar buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
 import { greeting } from "./script.js";
+import  loadSidebar from "./sidebar.js";
 
 console.log(greeting);
+loadSidebar()
---
+++ src/sidebar.js
@@ -0,0 +1,29 @@
+export default function() {
+  const sidebar = document.querySelector(".sidebar");
+
+  const heading = document.createElement("h1");
+  heading.classList.add("sidebar-heading");
+  heading.textContent = "TO-DO";
+
+  const home = document.createElement("button");
+  home.classList.add(".sidebar-home");
+  home.textContent = "Home";
+
+  const todo = document.createElement("button");
+  todo.classList.add(".sidebar-todo");
+  todo.textContent = "To do";
+
+  const projects = document.createElement("button");
+  projects.classList.add(".sidebar-projects");
+  projects.textContent = "Projects";
+
+  const addProject = document.createElement("button");
+  addProject.classList.add(".sidebar-addProject");
+  addProject.textContent = "+ Add Project";
+
+  sidebar.appendChild(heading);
+  sidebar.appendChild(home);
+  sidebar.appendChild(todo);
+  sidebar.appendChild(projects);
+  sidebar.append(addProject);
+}
