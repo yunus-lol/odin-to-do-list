3f5e62456a2ee98641c60696e7d426e63f7a571c
Yunus Ahmed <YunusAhmed1266@gmail.com>
2026-01-05 16:59:32 +0000

    Deployment commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# odin-to-do-list
-this is a to do app using html, css, javascript and webpack
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./styles.css";
-import { generateModalContent } from "./projects-modal.js";
-import { showProject } from "./to-dos.js";
-
-generateModalContent();
-showProject(0)
